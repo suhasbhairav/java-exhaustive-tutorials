c0cdfeba15dbdb918f8af9fb2eff333f6abadf14
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-18 00:47:01 +0200

    Inheritance

---
+++ Java-Tutorials/src/com/tutorials/learn/inheritance/Employee.java
@@ -0,0 +1,15 @@
+package com.tutorials.learn.inheritance;
+
+public class Employee {
+	
+	private String name;
+	private int id;
+	private String dept;
+	
+	public Employee(String name, int id, String dept) {
+		this.name = name;
+		this.id = id;
+		this.dept = dept;
+	}
+
+}
