7e0d1ab01c08f87e10f687dfa139f0b81e2ec311
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-18 18:53:54 +0200

    Add static import

---
+++ Java-Tutorials/src/com/tutorials/learn/methods/StaticImports.java
@@ -0,0 +1,11 @@
+package com.tutorials.learn.methods;
+
+import static java.lang.Math.random;
+
+public class StaticImports {
+	public static void main(String[] args) {
+		double value = random();
+		System.out.println(value);
+	}
+
+}
