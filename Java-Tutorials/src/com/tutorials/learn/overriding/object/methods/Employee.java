222eb4efeaf5248c6a20b2662eb64c18b77c0f6c
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-18 17:54:54 +0200

    Add equals overriding

--- Java-Tutorials/src/com/tutorials/learn/overriding/object/methods/Employee.java
+++ Java-Tutorials/src/com/tutorials/learn/overriding/object/methods/Employee.java
@@ -14,4 +14,16 @@ public class Employee {
 	public String toString() {
 		return this.name + " ; " + this.address;
 	}
+	
+	@Override
+	public boolean equals(Object o) {
+		boolean res = false;
+		if(o != null && o instanceof Employee) {
+			if(o.toString().contains("a")) {
+				res = true;
+			}
+		}
+		
+		return res;
+	}
 }
