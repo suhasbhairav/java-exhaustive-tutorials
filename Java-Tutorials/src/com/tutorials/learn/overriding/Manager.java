e69173868481e7f8337837dba2781c40a3bd1cae
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-18 17:29:22 +0200

    Update overriding

--- Java-Tutorials/src/com/tutorials/learn/overriding/Manager.java
+++ Java-Tutorials/src/com/tutorials/learn/overriding/Manager.java
@@ -12,7 +12,18 @@ public class Manager extends Employee {
 	
 	@Override
 	public String getDetails() {
-		return super.getDetails() + " ; " + this.deptName;
+		return super.getDetails() + " ; manager = " + this.deptName;
+	}
+	
+	public static void main(String[] args) {
+		Employee e = new Employee("Suhas", 10, "London");
+		System.out.println(e.getDetails());
+		
+		Employee m = new Manager("Ajay", 20, "London", "Street");
+		System.out.println(m.getDetails());
+		
+		Employee admin = new Administrator("Raj", 30, "London", "Awesome");
+		System.out.println(admin.getDetails());
 	}
 
 }
