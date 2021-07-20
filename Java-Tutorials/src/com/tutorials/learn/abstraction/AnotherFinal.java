6404ded121c92100c799ca24b8285cb48181cadf
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-20 13:14:18 +0200

    Add abstraction

---
+++ Java-Tutorials/src/com/tutorials/learn/abstraction/AnotherFinal.java
@@ -0,0 +1,12 @@
+package com.tutorials.learn.abstraction;
+
+public class AnotherFinal {
+	
+	public void display() {
+		//final variable can be declared inside the methods as well
+		//and cannot be changed as per the definition
+		final int var = 100;
+		System.out.println(var);
+	}
+
+}
