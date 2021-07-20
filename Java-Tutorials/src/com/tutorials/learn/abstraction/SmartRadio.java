5085fb6be716827bfe1843c159ae6cc7c39847e2
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-20 13:15:06 +0200

    Update abstraction

---
+++ Java-Tutorials/src/com/tutorials/learn/abstraction/SmartRadio.java
@@ -0,0 +1,11 @@
+package com.tutorials.learn.abstraction;
+
+//Cannot be subclassed
+public final class SmartRadio {
+	
+	//Cannot be inherited
+	public final void display() {
+		System.out.println("Smart Radio...");
+	}
+
+}
